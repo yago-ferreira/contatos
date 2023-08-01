bd098b0379ce72e797ef90c91fe4a516bc02f97b
yago-ferreira <yago.rox@hotmail.com>
2023-08-01 15:30:36 -0300

    mudando a rota do express

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const mysql = require('mysql2');
-const port = 3306; // Escolha a porta que desejar
+const port = 999; // Escolha a porta que desejar
 
 // Configuração do banco de dados
 const db = mysql.createConnection({
@@ -9,6 +9,7 @@ const db = mysql.createConnection({
   user: 'root',
   password: 'Yago9259588!',
   database: 'agenda',
+  port: 3306, // Porta do MySQL (padrão é 3306)
 });
 
 // Conectar ao banco de dados
@@ -18,7 +19,6 @@ db.connect((err) => {
 });
 
 // Rotas para manipular o CRUD
-// Exemplo de rota para buscar todos os registros de uma tabela
 app.get('/api/registros', (req, res) => {
   const sql = 'SELECT * FROM contato';
   db.query(sql, (err, result) => {
@@ -27,7 +27,6 @@ app.get('/api/registros', (req, res) => {
   });
 });
 
-// Mais rotas para realizar as operações de CRUD
 
 // Inicie o servidor
 app.listen(port, () => {
